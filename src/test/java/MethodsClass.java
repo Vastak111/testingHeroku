33c9235b5c2bc8b246801acc34256c55e547f00b
valerii.liasotskyi <valerii.liasotskyi@ab-soft.net>
2020-12-09 22:06:25 +0200

    Added test2

--- src/test/java/MethodsClass.java
+++ src/test/java/MethodsClass.java
@@ -2,8 +2,13 @@ public class MethodsClass {
 
     private String herokuAppAddress = "http://the-internet.herokuapp.com/";
     private String removeElementsAddress = "Add/Remove Elements";
-    private String addElementButton = "button";
-    private String addedManually = "added-manually";
+    private String addElementButton = "//button[contains(text(),'Add Element')]";
+    private String addedManually = "//button[contains(text(),'Delete')]";
+    private String javaScriptAlertsLink = "JavaScript Alerts";
+    private String clickForJSAlertXPath = "//button[contains(text(),'Click for JS Alert')]";
+    private String alertResult = "//p[@id='result']";
+    private String clickForJSConfirm = "//button[contains(text(),'Click for JS Confirm')]";
+    private String clickForJSPrompt = "//button[contains(text(),'Click for JS Prompt')]";
 
     public String getHerokuAppAddress() {
         return herokuAppAddress;
@@ -19,5 +24,24 @@ public class MethodsClass {
 
     public String getAddedManually() {
         return addedManually;
+    }
+
+    public String getJavaScriptAlertsLink() {
+        return javaScriptAlertsLink;
+    }
+
+    public String getClickForJSAlertXPath() {
+        return clickForJSAlertXPath;
+    }
+    public String getAlertResult() {
+        return alertResult;
+    }
+
+    public String getClickForJSConfirm() {
+        return clickForJSConfirm;
+    }
+
+    public String getClickForJSPrompt() {
+        return clickForJSPrompt;
     }
 }
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,20 +1,17 @@
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-
-import static org.testng.Assert.assertEquals;
 
 public class Tests extends ParentClass {
 
     MethodsClass method = new MethodsClass();
 
     @Test
-    public void test1() throws InterruptedException, IOException {
+    public void test1() throws InterruptedException {
 
         chromeDriver.get(method.getHerokuAppAddress());
         chromeDriver.manage().window().maximize();
@@ -22,19 +19,73 @@ public class Tests extends ParentClass {
         WebElement addRemoveElements = chromeDriver.findElement(By.linkText(method.getRemoveElementsAddress()));
         addRemoveElements.click();
 
-        WebElement addElementButton = chromeDriver.findElementByCssSelector(method.getAddElementButton());
+        WebElement addElementButton = chromeDriver.findElementByXPath(method.getAddElementButton());
         for (int i = 0; i <= 2; i++) {
             addElementButton.click();
             Thread.sleep(1000); //waiting for elements to appear
         }
 
-        List<WebElement> deleteButtons = chromeDriver.findElementsByClassName(method.getAddedManually());
+        List<WebElement> deleteButtons = chromeDriver.findElementsByXPath(method.getAddedManually());
         Assert.assertEquals(deleteButtons.size(), 3, "There are NOT 3 delete buttons");
 
         deleteButtons.get(0).click();
         Thread.sleep(500); //waiting for element to disappear
-        deleteButtons = chromeDriver.findElementsByClassName(method.getAddedManually());
+        deleteButtons = chromeDriver.findElementsByXPath(method.getAddedManually());
         Assert.assertEquals(deleteButtons.size(), 2, "There are NOT 2 elements left");
+    }
+
+    @Test
+    public void test2() throws InterruptedException {
+
+        chromeDriver.get(method.getHerokuAppAddress());
+        chromeDriver.manage().window().maximize();
+
+        WebElement javaScriptAlerts = chromeDriver.findElement(By.linkText(method.getJavaScriptAlertsLink()));
+        javaScriptAlerts.click();
+
+       try {
+           WebElement clickForJSAlertButton = chromeDriver.findElementByXPath(method.getClickForJSAlertXPath());
+        clickForJSAlertButton.click();
+        Thread.sleep(500); //waiting for element to appear
+        chromeDriver.switchTo().alert().accept();
+       } catch (TimeoutException e) {
+           System.out.println("The alert hasn't appeared");
+       }
+        Thread.sleep(500); //waiting for text to appear
+
+        WebElement alertResult = chromeDriver.findElementByXPath(method.getAlertResult());
+        Assert.assertEquals(alertResult.getText(), "You successfuly clicked an alert",
+                "The Result text is wrong");
+
+        try {
+            WebElement clickForJSConfirm = chromeDriver.findElementByXPath(method.getClickForJSConfirm());
+            clickForJSConfirm.click();
+            Thread.sleep(500);//waiting for element to appear
+            chromeDriver.switchTo().alert().dismiss();
+        } catch (TimeoutException e) {
+            System.out.println("The alert hasn't appeared");
+        }
+        Thread.sleep(500); //waiting for text to appear
+
+        alertResult = chromeDriver.findElementByXPath(method.getAlertResult());
+        Assert.assertEquals(alertResult.getText(), "You clicked: Cancel",
+                "The Result text is wrong");
+
+        try {
+            WebElement clickForJSPrompt = chromeDriver.findElementByXPath(method.getClickForJSPrompt());
+            clickForJSPrompt.click();
+            Thread.sleep(500);//waiting for element to appear
+            Alert alert = chromeDriver.switchTo().alert();
+            alert.sendKeys("asd");
+            alert.accept();
+        } catch (TimeoutException e) {
+            System.out.println("The alert hasn't appeared");
+        }
+        Thread.sleep(500); //waiting for text to appear
+
+        alertResult = chromeDriver.findElementByXPath(method.getAlertResult());
+        Assert.assertEquals(alertResult.getText(), "You entered: asd",
+                "The Result text is wrong");
     }
 
 
